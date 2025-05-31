4e793e2f4528d3fef856f57f9ee120a31002cb65
0xminer11 <arali.naveen1@gmail.com>
2025-05-31 22:42:41 +0530

    added psm contracts

---
+++ src/PSM.sol
@@ -0,0 +1,82 @@
+pragma solidity 0.8.20;
+
+import "./Stablecoin.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+contract psm is OwnableUpgradeable{
+
+Stablecoin public immutable stable;
+Stablecoin public immutable underlying;
+
+uint256 public immutable DECIMAL_DIFF;
+
+error NotEnoughLiquidity(address contractAddress,uint256 StableLiquidity, uint256 stableAmount);
+
+constructor(address _stable, address _underlaying){
+    stable = Stablecoin(_stable);
+    underlying = Stablecoin(_underlaying);
+
+    DECIMAL_DIFF =10 ** (stable.decimals() -Stablecoin(underlying).decimals());
+}
+
+
+function mint(address _to,uint256 _underlyingAmount) external returns (uint256 stableAmount){
+    uint256 balance = underlying.balanceOf(address(this));
+    
+    underlying.transferFrom(msg.sender,address(this),_underlyingAmount);
+
+    uint256 transferredUnderlyingAmount = underlying.balanceOf(address(this)) - balance;
+
+    stableAmount = _underlyingToStable(transferredUnderlyingAmount);
+
+    uint256 StableLiquidity = stable.balanceOf(address(this));
+
+    if(StableLiquidity < stableAmount) revert NotEnoughLiquidity(address(this),StableLiquidity,stableAmount);
+
+    stable.transfer(_to, stableAmount);
+}
+
+
+
+function redeem(address _to, uint256 _underlyingAmount) external returns(uint256 stableAmount){
+    stableAmount = _stableToUnderlying(_underlyingAmount);
+
+    stable.transferFrom(msg.sender,address(this), stableAmount);
+
+    uint256 underlyingLiquidity = underlying.balanceOf(address(this));
+
+    if (underlyingLiquidity < _underlyingAmount) revert NotEnoughLiquidity(address(this),underlyingLiquidity,_underlyingAmount);
+
+    underlying.transfer(_to, _underlyingAmount);
+
+}
+
+function underlyingToStable(uint256 _underlyingAmount) external view returns(uint256){
+    return _underlyingToStable(_underlyingAmount);
+}
+
+function _underlyingToStable(uint256 _underlyingAmount) internal view returns(uint256){
+    return _underlyingAmount * DECIMAL_DIFF;
+}
+
+function stableToUnderlying(uint256 _underlyingAmount) external view returns(uint256){
+    return _underlyingToStable(_underlyingAmount);
+}
+
+function _stableToUnderlying(uint256 _underlyingAmount) internal view returns(uint256){
+    return _underlyingAmount / DECIMAL_DIFF;
+}
+
+
+function getUnderlyingLiquidity() external view returns(uint256 _underlyingAmount){
+    return(underlying.balanceOf(address(this)));
+}
+
+function getStableLiquidity() external view returns(uint256 stableAmount){
+    return(stable.balanceOf(address(this)));
+}
+
+function removeLiquidity(uint256 _stableAmount) external onlyOwner {
+    stable.transfer(msg.sender,_stableAmount);
+}
+
+}
---
+++ src/Stablecoin.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.20;
+
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+
+contract Stablecoin  is ERC20Upgradeable , OwnableUpgradeable{
+    constructor(string memory name,string memory symbol){
+        __ERC20_init(name, symbol);
+        __Ownable_init(msg.sender);
+    }   
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
